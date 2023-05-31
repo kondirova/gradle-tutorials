60c02298838e595529f9769f4ef2a56286f5b2db
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2023-05-31 19:31:34 +0200

    added toolchain example

---
+++ toolchain/src/test/java/org/example/TestCase.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class TestCase {
+    @Test
+    public void test() {
+        System.out.println("Test java.home: " + System.getProperty("java.home"));
+    }
+}
